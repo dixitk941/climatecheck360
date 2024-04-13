881a86b90d9e8b76d51de3a6a971d184027acee2
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-04-13 09:50:58 +0530

    Update forcast.js

--- src/forcast.js
+++ src/forcast.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import apiKeys from "./apiKeys";
 import ReactAnimatedWeather from "react-animated-weather";
@@ -11,9 +11,7 @@ function Forcast(props) {
   const search = (city) => {
     axios
       .get(
-        `${apiKeys.base}weather?q=${
-          city != "[object Object]" ? city : query
-        }&units=metric&APPID=${apiKeys.key}`
+        `${apiKeys.base}weather?q=${city}&units=metric&APPID=${apiKeys.key}`
       )
       .then((response) => {
         setWeather(response.data);
@@ -23,20 +21,8 @@ function Forcast(props) {
         console.log(error);
         setWeather("");
         setQuery("");
-        setError({ message: "Not Found", query: query });
+        setError("Not Found");
       });
-  };
-  function checkTime(i) {
-    if (i < 10) {
-      i = "0" + i;
-    } // add zero in front of numbers < 10
-    return i;
-  }
-
-  const defaults = {
-    color: "white",
-    size: 112,
-    animate: true,
   };
 
   useEffect(() => {
@@ -48,9 +34,9 @@ function Forcast(props) {
       <div className="forecast-icon">
         <ReactAnimatedWeather
           icon={props.icon}
-          color={defaults.color}
-          size={defaults.size}
-          animate={defaults.animate}
+          color="white"
+          size={112}
+          animate={true}
         />
       </div>
       <div className="today-weather">
@@ -64,17 +50,16 @@ function Forcast(props) {
             value={query}
           />
           <div className="img-box">
-            {" "}
             <img
               src="https://images.avishkaar.cc/workflow/newhp/search-white.png"
-              onClick={search}
+              alt="Search Icon"
+              onClick={() => search(query)}
             />
           </div>
         </div>
         <ul>
-          {typeof weather.main != "undefined" ? (
-            <div>
-              {" "}
+          {typeof weather.main !== "undefined" ? (
+            <>
               <li className="cityHead">
                 <p>
                   {weather.name}, {weather.sys.country}
@@ -82,6 +67,7 @@ function Forcast(props) {
                 <img
                   className="temp"
                   src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+                  alt={weather.weather[0].description}
                 />
               </li>
               <li>
@@ -108,15 +94,14 @@ function Forcast(props) {
                   {Math.round(weather.wind.speed)} Km/h
                 </span>
               </li>
-            </div>
+            </>
           ) : (
-            <li>
-              {error.query} {error.message}
-            </li>
+            <li>{error}</li>
           )}
         </ul>
       </div>
     </div>
   );
 }
+
 export default Forcast;
