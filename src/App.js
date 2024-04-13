03139cba1282d87bbbc3a4576b9389147bb9742f
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-04-13 12:18:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,20 +10,13 @@ import './App.css';
 // Initialize Firebase
 const firebaseConfig = {
    apiKey: "AIzaSyDLT5VGNrm5_Ks9gs8YlBvvS9rKrKjD2sY",
-
   authDomain: "climate-check-360.firebaseapp.com",
-
   databaseURL: "https://climate-check-360-default-rtdb.firebaseio.com",
-
   projectId: "climate-check-360",
-
   storageBucket: "climate-check-360.appspot.com",
-
   messagingSenderId: "491332887364",
-
   appId: "1:491332887364:web:6da35e591994f98f54ad11",
-
-  measurementId: "G-WL33DTYDFY",
+  measurementId: "G-WL33DTYDFY"
 
 };
 
