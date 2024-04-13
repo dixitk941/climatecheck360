6bdffb06acb52dfd35c710f0fce1f7677563e45b
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2024-04-13 09:41:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,19 +8,6 @@ function App() {
       <div className="container">
         <CurrentLocation />
       </div>
-      <div className="footer-info">
-        <a href="https://www.htmlhints.com/article/how-to-create-toggle-switch/93">
-          Download Source Code
-        </a>{" "}
-        | Developed by{" "}
-        <a target="_blank" href="https://www.gauravghai.dev/">
-          Gaurav Ghai
-        </a>{" "}
-        | Powered by{" "}
-        <a target="_blank" href="https://www.htmlhints.com/">
-          HTML HINTS
-        </a>
-      </div>
     </React.Fragment>
   );
 }
